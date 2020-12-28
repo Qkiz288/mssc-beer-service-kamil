2bb2fde4c73b2333b51afff077822d086366d426
Kamil <qkiz288@gmail.com>
2020-12-28 15:41:26 +0100

    Added Mapstruct mappers, closes #12

---
+++ src/main/java/com/kkukielka/mappers/BeerMapper.java
@@ -0,0 +1,13 @@
+package com.kkukielka.mappers;
+
+import com.kkukielka.web.domain.Beer;
+import com.kkukielka.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = DateMapper.class)
+public interface BeerMapper {
+
+    BeerDto beerToBeerDto(Beer beer);
+
+    Beer beerDtoToBeer(BeerDto dto);
+}
