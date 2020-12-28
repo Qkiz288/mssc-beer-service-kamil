09c49c1989132bf3a869d070ce22f778d4d58aa9
Kamil <qkiz288@gmail.com>
2020-12-28 13:07:42 +0100

    Added Beer repository, closes #7

---
+++ src/main/java/com/kkukielka/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.kkukielka.repositories;
+
+import com.kkukielka.web.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
