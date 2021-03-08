c169fa0a45c56e95be07ea74ae242539584604be
Kamil <qkiz288@gmail.com>
2021-03-08 09:48:40 +0100

    Inventory service failover client

---
+++ src/main/java/com/kkukielka/services/inventory/InventoryFailoverFeignClient.java
@@ -0,0 +1,15 @@
+package com.kkukielka.services.inventory;
+
+import com.kkukielka.services.inventory.model.BeerInventoryDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@FeignClient(name = "inventory-failover")
+public interface InventoryFailoverFeignClient {
+    @RequestMapping(method = RequestMethod.GET, value = "/inventory-failover")
+    ResponseEntity<List<BeerInventoryDto>> getOnhandInventory();
+}
